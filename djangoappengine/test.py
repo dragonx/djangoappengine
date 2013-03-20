49931042b55c20991a38f57732d0fd3243637684
projecteat <projecteatapp@gmail.com>
2013-03-20 12:04:32 -0400

    Add support for devappserver2 to LiveServerTestCase. Can currently only run one test at a time, since server sometimes fails to shut down.

--- djangoappengine/test.py
+++ djangoappengine/test.py
@@ -5,18 +5,16 @@ import threading
 import httplib
 import socket
 import select
-from django.core.handlers.wsgi import WSGIHandler
-from django.core.servers.basehttp import WSGIServerException
 from django.db import connections
 from django.test import TestCase, TransactionTestCase
-from django.test.testcases import _MediaFilesHandler
-from django.contrib.staticfiles.handlers import StaticFilesHandler
 
 from google.appengine.datastore import datastore_stub_util
 
 from db.stubs import stub_manager
 from utils import appid
 from unittest.runner import TextTestResult
+
+from boot import devappserver_ver
 
 class GAETestCase(TestCase):
     '''
@@ -55,7 +53,7 @@ class SyncTextTestResult(TextTestResult):
             test.server_thread.join()
         super(SyncTextTestResult, self).addError(test, err)
 
-try:
+if devappserver_ver == 1:
     from google.appengine.tools import dev_appserver
     from google.appengine.tools import dev_appserver_main
 
@@ -170,88 +168,200 @@ try:
                     pass
             super(LiveServerThread, self).join(timeout)
 
-except ImportError:
-    # can't use dev_appserver in devappserver2
-    pass
 
-# This is copied directly from django.test.testcases
-class LiveServerTestCase(TransactionTestCase):
-    """
-    Does basically the same as TransactionTestCase but also launches a live
-    http server in a separate thread so that the tests may use another testing
-    framework, such as Selenium for example, instead of the built-in dummy
-    client.
+    # This is copied directly from django.test.testcases
+    class LiveServerTestCase(TransactionTestCase):
+        """
+        Does basically the same as TransactionTestCase but also launches a live
+        http server in a separate thread so that the tests may use another testing
+        framework, such as Selenium for example, instead of the built-in dummy
+        client.
 
-    Note that it inherits from TransactionTestCase instead of TestCase because
-    the threads do not share the same transactions (unless if using in-memory
-    sqlite) and each thread needs to commit all their transactions so that the
-    other thread can see the changes.
+        Note that it inherits from TransactionTestCase instead of TestCase because
+        the threads do not share the same transactions (unless if using in-memory
+        sqlite) and each thread needs to commit all their transactions so that the
+        other thread can see the changes.
 
-    Be careful that almost everything you do needs to be synchronized against
-    the liveServerLock (which you can easily reference as this.lock()
-    """
-    lock = liveServerLock
+        Be careful that almost everything you do needs to be synchronized against
+        the liveServerLock (which you can easily reference as this.lock()
+        """
+        lock = liveServerLock
 
-    @property
-    def live_server_url(self):
-        return 'http://%s:%s' % (
-            self.server_thread.host, self.server_thread.port)
+        @property
+        def live_server_url(self):
+            return 'http://%s:%s' % (
+                self.server_thread.host, self.server_thread.port)
 
-    def _pre_setup(self):
-        connections_override = {}
-        for conn in connections.all():
-            # If using in-memory sqlite databases, pass the connections to
-            # the server thread.
-            if (conn.settings_dict['ENGINE'] == 'django.db.backends.sqlite3'
-                and conn.settings_dict['NAME'] == ':memory:'):
-                # Explicitly enable thread-shareability for this connection
-                conn.allow_thread_sharing = True
-                connections_override[conn.alias] = conn
+        def _pre_setup(self):
+            connections_override = {}
+            for conn in connections.all():
+                # If using in-memory sqlite databases, pass the connections to
+                # the server thread.
+                if (conn.settings_dict['ENGINE'] == 'django.db.backends.sqlite3'
+                    and conn.settings_dict['NAME'] == ':memory:'):
+                    # Explicitly enable thread-shareability for this connection
+                    conn.allow_thread_sharing = True
+                    connections_override[conn.alias] = conn
 
-        # Launch the live server's thread
-        specified_address = os.environ.get(
-            'DJANGO_LIVE_TEST_SERVER_ADDRESS', 'localhost:8081-8089')
+            # Launch the live server's thread
+            specified_address = os.environ.get(
+                'DJANGO_LIVE_TEST_SERVER_ADDRESS', 'localhost:8081-8089')
 
-        # The specified ports may be of the form '8000-8010,8080,9200-9300'
-        # i.e. a comma-separated list of ports or ranges gg ports, so we break
-        # it down into a detailed list of all possible ports.
-        possible_ports = []
-        try:
-            host, port_ranges = specified_address.split(':')
-            for port_range in port_ranges.split(','):
-                # A port range can be of either form: '8000' or '8000-8010'.
-                extremes = map(int, port_range.split('-'))
-                assert len(extremes) in [1, 2]
-                if len(extremes) == 1:
-                    # Port range of the form '8000'
-                    possible_ports.append(extremes[0])
-                else:
-                    # Port range of the form '8000-8010'
-                    for port in range(extremes[0], extremes[1] + 1):
-                        possible_ports.append(port)
-        except Exception:
-            raise ImproperlyConfigured('Invalid address ("%s") for live '
-                'server.' % specified_address)
+            # The specified ports may be of the form '8000-8010,8080,9200-9300'
+            # i.e. a comma-separated list of ports or ranges gg ports, so we break
+            # it down into a detailed list of all possible ports.
+            possible_ports = []
+            try:
+                host, port_ranges = specified_address.split(':')
+                for port_range in port_ranges.split(','):
+                    # A port range can be of either form: '8000' or '8000-8010'.
+                    extremes = map(int, port_range.split('-'))
+                    assert len(extremes) in [1, 2]
+                    if len(extremes) == 1:
+                        # Port range of the form '8000'
+                        possible_ports.append(extremes[0])
+                    else:
+                        # Port range of the form '8000-8010'
+                        for port in range(extremes[0], extremes[1] + 1):
+                            possible_ports.append(port)
+            except Exception:
+                raise ImproperlyConfigured('Invalid address ("%s") for live '
+                    'server.' % specified_address)
 
-        self.server_thread = LiveServerThread(
-            host, possible_ports, connections_override)
-        self.server_thread.daemon = True
-        self.server_thread.start()
+            self.server_thread = LiveServerThread(
+                host, possible_ports, connections_override)
+            self.server_thread.daemon = True
+            self.server_thread.start()
 
-        # Wait for the live server to be ready
-        self.server_thread.is_ready.wait()
-        if self.server_thread.error:
-            raise self.server_thread.error
+            # Wait for the live server to be ready
+            self.server_thread.is_ready.wait()
+            if self.server_thread.error:
+                raise self.server_thread.error
 
-        liveServerLock.acquire() # Lock while we load fixtures
-        super(LiveServerTestCase, self)._pre_setup()
-        liveServerLock.release()
+            liveServerLock.acquire() # Lock while we load fixtures
+            super(LiveServerTestCase, self)._pre_setup()
+            liveServerLock.release()
 
-    def _post_teardown(self):
-        # There may not be a 'server_thread' attribute if setUpClass() for some
-        # reasons has raised an exception.
-        if hasattr(self, 'server_thread'):
-            # Terminate the live server's thread
-            self.server_thread.join()
-        super(LiveServerTestCase, self)._post_teardown()
+        def _post_teardown(self):
+            # There may not be a 'server_thread' attribute if setUpClass() for some
+            # reasons has raised an exception.
+            if hasattr(self, 'server_thread'):
+                # Terminate the live server's thread
+                self.server_thread.join()
+            super(LiveServerTestCase, self)._post_teardown()
+
+else: # devappserver2
+    import sys
+    from django.db import DEFAULT_DB_ALIAS
+    from django.core.management import call_command
+    import dev_appserver
+    from google.appengine.api import apiproxy_stub_map
+    from google.appengine.tools.devappserver2 import devappserver2
+    from google.appengine.tools.devappserver2 import python_runtime
+    from google.appengine.tools.devappserver2 import shutdown
+
+    sys.path[1:1] = dev_appserver._DEVAPPSERVER2_PATHS
+
+    class LiveServerTestCase(TransactionTestCase):
+        """
+        Does basically the same as TransactionTestCase but also launches a live
+        http server in a separate thread so that the tests may use another testing
+        framework, such as Selenium for example, instead of the built-in dummy
+        client.
+
+        Note that it inherits from TransactionTestCase instead of TestCase because
+        the threads do not share the same transactions (unless if using in-memory
+        sqlite) and each thread needs to commit all their transactions so that the
+        other thread can see the changes.
+
+        Be careful that almost everything you do needs to be synchronized against
+        the liveServerLock (which you can easily reference as this.lock()
+        """
+        lock = liveServerLock
+
+        @property
+        def live_server_url(self):
+            return 'http://%s:%s' % (
+                'localhost', self.port)
+
+        def _pre_setup(self):
+            apiproxy_stub_map.apiproxy = apiproxy_stub_map.APIProxyStubMap()
+            self.server = None
+
+            # Launch the live server's thread
+            specified_address = os.environ.get(
+                'DJANGO_LIVE_TEST_SERVER_ADDRESS', 'localhost:8081-8089')
+
+            # The specified ports may be of the form '8000-8010,8080,9200-9300'
+            # i.e. a comma-separated list of ports or ranges gg ports, so we break
+            # it down into a detailed list of all possible ports.
+            possible_ports = []
+            try:
+                host, port_ranges = specified_address.split(':')
+                for port_range in port_ranges.split(','):
+                    # A port range can be of either form: '8000' or '8000-8010'.
+                    extremes = map(int, port_range.split('-'))
+                    assert len(extremes) in [1, 2]
+                    if len(extremes) == 1:
+                        # Port range of the form '8000'
+                        possible_ports.append(extremes[0])
+                    else:
+                        # Port range of the form '8000-8010'
+                        for port in range(extremes[0], extremes[1] + 1):
+                            possible_ports.append(port)
+            except Exception:
+                raise ImproperlyConfigured('Invalid address ("%s") for live '
+                    'server.' % specified_address)
+
+            python_runtime._RUNTIME_ARGS = [
+                sys.executable,
+                os.path.join(
+                    os.path.dirname(dev_appserver.__file__), '_python_runtime.py')]
+            options = devappserver2.PARSER.parse_args([
+                '--admin_port', '0',
+                '--port', '%s' % possible_ports[0],
+                '--datastore_path', ':memory:',
+                '--logs_path', ':memory:',
+                '--skip_sdk_update_check',
+                '--',
+                '.'])
+            self.port = possible_ports[0]
+            self.server = devappserver2.DevelopmentServer()
+            self.server.start(options)
+            
+            # Wait for the live server to be ready
+            # How do we sync this?
+
+            liveServerLock.acquire() # Lock while we load fixtures
+            super(LiveServerTestCase, self)._pre_setup()
+            liveServerLock.release()
+
+        def _fixture_setup(self):
+            # If the test case has a multi_db=True flag, flush all databases.
+            # Otherwise, just flush default.
+            if getattr(self, 'multi_db', False):
+                databases = connections
+            else:
+                databases = [DEFAULT_DB_ALIAS]
+            try:
+                for db in databases:
+                    # call_command('flush', verbosity=0, interactive=False, database=db)
+
+                    if hasattr(self, 'fixtures'):
+                        # We have to use this slightly awkward syntax due to the fact
+                        # that we're using *args and **kwargs together.
+                        call_command('loaddata', *self.fixtures,
+                                     **{'verbosity': 0, 'database': db})
+            except Exception, e:
+                pass
+
+        def _post_teardown(self):
+            if self.server:
+                print 'Stopping server'
+                self.server.stop()
+                print 'Server stopped'
+                import eat
+                eat.gaebp()
+
+            super(LiveServerTestCase, self)._post_teardown()
 
