64c4e640e155c5737dfe654a78c4a5e082558b5e
projecteat <projecteatapp@gmail.com>
2013-03-25 12:21:23 -0400

    Revert changes to utils.py that were needed for the standalone devapppserver2. Make python manage.py deploy work.  Removed the use of the appcfg -R flag, it shouldn't be necessary.

--- djangoappengine/utils.py
+++ djangoappengine/utils.py
@@ -9,23 +9,15 @@ have_appserver = bool(apiproxy_stub_map.apiproxy.GetStub('datastore_v3'))
 if have_appserver:
     appid = get_application_id()
 else:
-    from boot import devappserver_ver
-    if devappserver_ver == 1:
-        # Original dev_appserver method
+    try:
         from google.appengine.tools import dev_appserver
         from .boot import PROJECT_DIR
         appconfig = dev_appserver.LoadAppConfig(PROJECT_DIR, {},
                                                 default_partition='dev')[0]
         appid = appconfig.application.split('~', 1)[-1]
-    else:
-        try:
-            from google.appengine.tools.devappserver2 import application_configuration
-            configuration = application_configuration.ApplicationConfiguration(["app.yaml"])
-            appid = configuration.app_id.split('~', 1)[-1]
-            
-        except Exception, e:
-            raise Exception("Could not get appid. Is your app.yaml file missing? "
-                            "Error was: %s" % e)
+    except Exception, e:
+        raise Exception("Could not get appid. Is your app.yaml file missing? "
+                        "Error was: %s" % e)
 
 on_production_server = have_appserver and \
     not os.environ.get('SERVER_SOFTWARE', '').lower().startswith('devel')
