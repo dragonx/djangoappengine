122b1fe2ff79e86511ace9c3c49afa48fde465c9
projecteat <projecteatapp@gmail.com>
2013-08-21 02:12:56 -0400

    Fix for last change not working properly on the original dev_appserver.  Oops.

--- djangoappengine/management/commands/loaddata.py
+++ djangoappengine/management/commands/loaddata.py
@@ -5,14 +5,21 @@ from google.appengine.datastore import datastore_stub_util
 
 class Command(OriginalCommand):
     def handle(self, *fixture_labels, **options):
+        retval = None
         # Temporarily change consistency policy to force apply loaded data
         datastore = apiproxy_stub_map.apiproxy.GetStub('datastore_v3')
 
-        orig_consistency_policy = datastore._consistency_policy
-        datastore.SetConsistencyPolicy(datastore_stub_util.PseudoRandomHRConsistencyPolicy(probability=1))
+        try:
+            # datastore._consistency_policy only exists in dev_appserver
+            # will throw exception in production
+            orig_consistency_policy = datastore._consistency_policy
+            datastore.SetConsistencyPolicy(datastore_stub_util.PseudoRandomHRConsistencyPolicy(probability=1))
+        except:
+            orig_consistency_policy = None
 
         retval = super(Command, self).handle(*fixture_labels, **options)
 
-        datastore.SetConsistencyPolicy(orig_consistency_policy)
+        if orig_consistency_policy:
+            datastore.SetConsistencyPolicy(orig_consistency_policy)
 
         return retval
